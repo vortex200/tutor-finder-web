5b97bef71490606ab9a95c811ffeb08339ce43e8
Esox <doomar999@gmail.com>
2020-10-27 21:37:44 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const title = "Hello this is loaded by babel";
+
+ReactDOM.render(<div>{title}</div>, document.getElementById("app"));

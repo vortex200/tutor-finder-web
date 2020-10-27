35a7ce1c99b2573cf666317ae70644b3d7a272ec
Esox <doomar999@gmail.com>
2020-10-27 21:56:31 +0200

    added fake test

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const title = "Hello this is loaded by babel";
+
+export default function App() {
+  return <div>{title}</div>;
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 
-const title = "Hello this is loaded by babel";
-
-ReactDOM.render(<div>{title}</div>, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById("app"));

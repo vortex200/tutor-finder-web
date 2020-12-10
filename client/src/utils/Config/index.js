bc1db4907ab03f8d977a3e78c4f4ce5fe4b5757e
Esox <doomar999@gmail.com>
2020-12-10 16:25:05 +0200

    small fix

--- client/src/utils/Config/index.js
+++ client/src/utils/Config/index.js
@@ -1,5 +1,5 @@
 const Config = {
-  BACKEND_URL: "http://localhost:5000/api",
+  BACKEND_URL: "",
   GOOGLE_CLIENT_ID:
     "458166213991-4oqjj5rubqfc3ipkv2utbk2r5rmj5fg3.apps.googleusercontent.com",
   FACEBOOK_APP_ID: "412134389833525",

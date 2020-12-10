e876750285cf78388d3ed5971389afcd03ecefce
Esox <doomar999@gmail.com>
2020-12-10 16:21:25 +0200

    added profile button

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,9 +1,39 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import axios from "axios";
+
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
 
 function NavBar() {
+  const auth = useSelector((state) => state.auth);
+
+  const { user, isLogged } = auth;
+
+  const handleLogout = async () => {
+    try {
+      await axios.get("/user/logout");
+      localStorage.removeItem("firstLogin");
+      window.location.href = "/";
+    } catch (err) {
+      window.location.href = "/";
+    }
+  };
+
+  const userLink = () => {
+    return (
+      <DropdownButton id="dropdown-basic-button" title={user.name}>
+        <Dropdown.Item href="/profile">Profile</Dropdown.Item>
+        <Dropdown.Item href="/" onClick={handleLogout}>
+          Logout
+        </Dropdown.Item>
+      </DropdownButton>
+    );
+  };
+
   return (
     <Navbar bg="light" expand="lg">
       <Container>
@@ -11,7 +41,11 @@ function NavBar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link href="/login">Prisijungti</Nav.Link>
+            {isLogged ? (
+              userLink()
+            ) : (
+              <Nav.Link href="/login">Prisijungti</Nav.Link>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>

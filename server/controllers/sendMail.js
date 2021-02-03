44a58ca5389a33cad2f3363537c370f569bc954c
Esox <doomar999@gmail.com>
2021-02-03 09:55:34 +0200

    mail provider change

--- server/controllers/sendMail.js
+++ server/controllers/sendMail.js
@@ -1,45 +1,20 @@
 const nodemailer = require("nodemailer");
-const { google } = require("googleapis");
-const { OAuth2 } = google.auth;
-const OAUTH_PLAYGROUND = "https://developers.google.com/oauthplayground";
+const mg = require("nodemailer-mailgun-transport");
 
-const {
-  MAILING_SERVICE_CLIENT_ID,
-  MAILING_SERVICE_CLIENT_SECRET,
-  MAILING_SERVICE_REFRESH_TOKEN,
-  SENDER_EMAIL_ADDRESS,
-} = process.env;
+const auth = {
+  auth: {
+    api_key: process.env.MAILGUN_API_KEY,
+    domain: process.env.MAILGUN_DOMAIN,
+  },
+};
 
-const oauth2Client = new OAuth2(
-  MAILING_SERVICE_CLIENT_ID,
-  MAILING_SERVICE_CLIENT_SECRET,
-  MAILING_SERVICE_REFRESH_TOKEN,
-  OAUTH_PLAYGROUND
-);
-
-// send mail
 const sendEmail = (to, url, txt) => {
-  oauth2Client.setCredentials({
-    refresh_token: MAILING_SERVICE_REFRESH_TOKEN,
-  });
-
-  const accessToken = oauth2Client.getAccessToken();
-  const smtpTransport = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      type: "OAuth2",
-      user: SENDER_EMAIL_ADDRESS,
-      clientId: MAILING_SERVICE_CLIENT_ID,
-      clientSecret: MAILING_SERVICE_CLIENT_SECRET,
-      refreshToken: MAILING_SERVICE_REFRESH_TOKEN,
-      accessToken,
-    },
-  });
+  const smtpTransport = nodemailer.createTransport(mg(auth));
 
   const mailOptions = {
-    from: SENDER_EMAIL_ADDRESS,
+    from: process.env.SENDER_EMAIL_ADDRESS,
     to: to,
-    subject: "Tutor Finder email verification",
+    subject: "Tutor Web Authentication email verification",
     html: `
             <div style="max-width: 700px; margin:auto; border: 10px solid #ddd; padding: 50px 20px; font-size: 110%;">
             <h2 style="text-align: center; text-transform: uppercase;color: teal;">Welcome to the Tutor Finder website.</h2>
@@ -57,6 +32,7 @@ const sendEmail = (to, url, txt) => {
   };
 
   smtpTransport.sendMail(mailOptions, (err, infor) => {
+    console.log(err, infor);
     if (err) return err;
     return infor;
   });

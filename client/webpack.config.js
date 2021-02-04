ea4ba695bfd209c9ff22deb144e152daaa189dab
Esox <doomar999@gmail.com>
2021-02-04 09:56:48 +0200

    fix

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: path.resolve("src", "index.js"),
   output: {
-    path: path.resolve(__dirname, "..", "build", "server"),
+    path: path.resolve(__dirname, "..", "server", "build"),
     filename: "bundle.js",
     publicPath: "/",
   },
